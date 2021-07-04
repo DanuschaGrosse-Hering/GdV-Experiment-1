89c9c85443a6fd1b07618416f04718381a556813
DanuschaGrosse-Hering <d.grosse-hering@web.de>
2021-07-04 20:06:56 +0200

    Update

--- stichprobengroesse.R
+++ stichprobengroesse.R
@@ -5,6 +5,7 @@ f1 <- function(x){
   p2 <- pt(q=-qt(df=2*(x-1),p=0.975),df=2*(x-1),ncp=sqrt(x/2))
   p1-p2
 }
-plot(f1(1:70),ylab ="W'keit Fehler 2. Art",xlab="Stichprobenumfang n",main = "Zweiseitiger Zweistichproben t-Test",type = "l")  
+plot(f1(1:70),ylab ="W'keit Fehler 2. Art",xlab="Stichprobenumfang n",
+     main = "Zweiseitiger Zweistichproben t-Test",type = "l")  
 abline(h=0.2,col="red")
 abline(v=17,col="red")
